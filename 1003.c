b21e637154d4887dd09ab955596f7e196d2c61af
vitorFlavio <vitorflavio14@gmail.com>
2022-11-15 16:55:05 -0300

    Accept

---
+++ 1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int A, B, SOMA = 0;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    SOMA = A + B;
+
+    printf("SOMA = %d\n", SOMA);
+
+    return 0;
+}
