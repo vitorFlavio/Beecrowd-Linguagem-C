69ebeedf7952ae1762daa2ac008985618c6d6cac
vitorFlavio <vitorflavio14@gmail.com>
2022-11-16 17:54:36 -0300

    Accepted

---
+++ 1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+ 
+    int valor1,valor2,produto;
+
+    scanf("%d %d",&valor1,&valor2);
+
+    produto=valor1*valor2;
+
+    printf("PROD = %d\n",produto);
+ 
+    return 0;
+}
